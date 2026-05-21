919b4a4a75b70c52021a45bc596f9525a602d4ed
JoshOrrMOnash <jorr0003@student.monash.edu>
2026-05-22 03:49:58 +1000

    Add files via upload

---
+++ ExtinctSOT.js
@@ -0,0 +1,16 @@
+var vg_1 = "ESOT.json";
+var vg_2 = "StateEL.json"
+
+var vg_3 = "EndSmap.json"
+
+vegaEmbed("#LineGraph", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#StateChart", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#EndMap", vg_3).then(function(result) {
+
+}).catch(console.error);
