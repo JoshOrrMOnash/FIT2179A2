f91adc6dff6ffcddf15f74f5b7041a9c3aa04ced
JoshOrrMOnash <jorr0003@student.monash.edu>
2026-05-28 17:28:15 +1000

    Add files via upload

--- ExtinctSOT.js
+++ ExtinctSOT.js
@@ -2,6 +2,14 @@ var vg_1 = "ESOT.json";
 var vg_2 = "StateEL.json"
 
 var vg_3 = "EndSmap.json"
+var vg_4 = "HeatMap.json"
+var vg_5 = "donut.json"
+var vg_6 = "AreaChar.json"
+var vg_7 = "GroupBar.json"
+var vg_8 = "ConservArea.json"
+var vg_9 = "ConsvMap.json"
+var vg_10 = "RadialB.json"
+var vg_11 = "Stream.json"
 
 vegaEmbed("#LineGraph", vg_1).then(function(result) {
 
@@ -14,3 +22,36 @@ vegaEmbed("#StateChart", vg_2).then(function(result) {
 vegaEmbed("#EndMap", vg_3).then(function(result) {
 
 }).catch(console.error);
+
+vegaEmbed("#HeatMap", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#Donut", vg_5).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#AreaChart", vg_6).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#GroupBar", vg_7).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#ConservArea", vg_8).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#ConsvMap", vg_9).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#RadB", vg_10).then(function(result) {
+
+}).catch(console.error);
+
+vegaEmbed("#Stream", vg_11).then(function(result) {
+
+}).catch(console.error);
+
